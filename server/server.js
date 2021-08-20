333390e4a597c4a8d1d476846a8487bf0910b085
Monchichi <monchichi@Monchichis-Air.lan>
2021-08-20 18:49:28 -0400

    added get and post buttons

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const routes = require('./routes/api.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../' ))); 
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
